a36c880f97934b96003da9e07b0d0f5e2a8dcf41
Jaesin22 <jaesin22@naver.com>
2023-06-18 23:45:14 +0900

    change style

---
+++ src/components/Head.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Head() {
+  return (
+    <div className="lg:flex lg:items-center lg:justify-between text-center bg-slate-900 mt-8">
+      <div className="min-w-0 flex-1">
+        <h2 className="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
+          짤 생성기
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Head;
---
+++ src/components/Screen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Screen() {
+  return <div className="box-content h-32 w-32 p-4 border-4"></div>;
+}
+
+export default Screen;
