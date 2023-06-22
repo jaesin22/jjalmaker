0e2dd571562bfcf121af3b17ff3962db705d6efe
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-06-22 18:03:00 +0900

    edit fontcolor design

--- src/components/FontColor.js
+++ src/components/FontColor.js
@@ -1,17 +1,46 @@
 import React, { useState } from "react";
+import { FontColorsOutlined } from "@ant-design/icons";
+import { ChromePicker } from "react-color";
 
 const FontColor = ({ onColorChange }) => {
   const [color, setColor] = useState("#ff0000");
+  const [showColorPicker, setShowColorPicker] = useState(false);
+
+  const handleColorIconClick = () => {
+    setShowColorPicker(!showColorPicker);
+  };
 
   const handleColorChange = (e) => {
-    setColor(e.target.value);
-    onColorChange(e.target.value);
+    setColor(e.hex);
+    onColorChange(e.hex);
+  };
+
+  const handleClose = () => {
+    setShowColorPicker(false);
   };
 
   return (
     <div className="flex flex-row mt-12 w-96">
-      <label className="flex-none ml-3">폰트 색상</label>
-      <input type="color" onChange={handleColorChange} className="flex ml-3" />
+      <FontColorsOutlined
+        onClick={handleColorIconClick}
+        color={color}
+        className=" absolute"
+      />
+      {showColorPicker && (
+        <div className="absolute mt-5">
+          <div
+            style={{
+              position: "fixed",
+              top: "0",
+              right: "0",
+              bottom: "0",
+              left: "0",
+            }}
+            onClick={handleClose}
+          />
+          <ChromePicker color={color} onChange={handleColorChange} />
+        </div>
+      )}
     </div>
   );
 };
